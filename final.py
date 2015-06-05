ec3865723d3468ec85f056350cdc61578cae13de
GIS-Admin <gis-admin@gisadmin-P6-2060IX.(none)>
2015-06-05 16:25:16 +0530

    Second Commit

--- final.py
+++ final.py
@@ -4,29 +4,61 @@ import record
 import os
 import subprocess
 
+a={}
+updates=[]
 
 
 
+def write_record(fileobject):
+	try:
+		b=record.create('')#creates record of MOD13Q1 home page
+		#pprint.pprint(b, width=1)
+		for data in b:
+			fileobject.write(data)	
+			fileobject.write("\t")	
+			fileobject.write(b[data])
+			fileobject.write("\n")
+	except :
+		print "Could not identify file object "+fileobject
 
-def write_record(filename):
-	b=record.create('')#creates record of MOD13Q1 home page
-	#pprint.pprint(b, width=1)
-	for data in b:
-		filename.write(data)	
-		filename.write("\t")	
-		filename.write(b[data])
-		filename.write("\n")
+def create_dict(filename):
+	
+	try:	
+		
+		text=open(filename,"r")
+		text.seek(0,0)
+		line=text.readlines()
+		for i in range(0,len(line)):
+			line[i]=line[i].translate(None,'\n')
+			content=line[i].partition("\t")#splits the line at the tab character
+			a[content[0]]=content[2]
+		#pprint.pprint(a,width=1)
+		text.close()
+		return a
+	except:
+		print "Could not read the contents of "+filename
 
-if __name__=='__main__ ':
-	if 0!=subprocess.call('ls | grep MOD13Q1*',shell=True):
+
+if __name__ == "__main__":
+	if 0!=subprocess.call('ls | grep MOD13Q1.txt',shell=True):#file doesnot exist
 		os.system("touch MOD13Q1.txt")
 		mod13q1=open("MOD13Q1.txt","r+")
 		write_record(mod13q1)
 		mod13q1.close()
+		create_dict("MOD13Q1.txt")
 	else :
 		os.system("touch MOD13Q1_new.txt")
-		mod13q1_new=open("MOD13Q1.txt_new","r+")
+		mod13q1_new=open("MOD13Q1_new.txt","r+")
 		write_record(mod13q1_new)
 		mod13q1_new.close()
-
+		oldrecord=create_dict("MOD13Q1.txt")
+		newrecord=create_dict("MOD13Q1_new.txt")
+		#pprint.pprint(oldrecord,width=1)
+		#pprint.pprint(newrecord,width=1)
+		for i in newrecord:
+			if oldrecord[i]!=newrecord[i]:
+				updates.append(i)
+		print updates
+				
+			
 	
---
+++ test.py
@@ -0,0 +1,4 @@
+if __name__ == "__main__":
+    print "i am running as an independent program"
+else:
+    print "i am running as an imported module"
